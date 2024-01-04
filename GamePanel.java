388c086a17b7a523fb947da9b2f1780edaf61cad
HusainR1 <hsr2198@rit.edu>
2024-01-05 01:50:10 +0400

    Filling the constructor and other 2 methods, making grid boxes for easier understanding, and added a missed method.

--- GamePanel.java
+++ GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,18 +29,41 @@ public class GamePanel extends JPanel implements ActionListener{
 
     //Constructor
     GamePanel(){
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     //Start Game Method
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
     //Paint Component Method
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
     //Draw Method
     public void draw(Graphics g){
+        //Drawing Grid Lines 
+        //X-Axis
+        for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+        }
+        //Y-Axis
+        for (int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++){
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE );
+        }
+
+
+    }
+    //New Apple Method
+    public void newApple(){
 
     }
     //Move Method   
