dd6ad6fa35ffb975783248e089852a7c0afa54b2
HusainR1 <hsr2198@rit.edu>
2024-01-05 12:21:16 +0400

    Worked on the Move Method

--- GamePanel.java
+++ GamePanel.java
@@ -59,16 +59,38 @@ public class GamePanel extends JPanel implements ActionListener{
         for (int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++){
             g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE );
         }
-
+        //Apple figures on the Screen
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
     }
     //New Apple Method
     public void newApple(){
-
+        //Creating random co-ordinates for the position of the Apple
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     //Move Method   
     public void move(){
+        for(int i = bodyParts;i>0;i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
 
+        switch(direction){
+            case 'U': //UP
+                y[0] = y[0]-UNIT_SIZE;
+                break;
+            case 'D'://DOWN
+                y[0] = y[0]+UNIT_SIZE;
+                break;
+            case 'L'://LEFT
+                x[0] = x[0]-UNIT_SIZE;
+                break;
+            case 'R'://RIGHT
+                x[0] = x[0]+UNIT_SIZE;
+                break;
+        }
     }
     //Check Apple Method
     public void checkApple(){
